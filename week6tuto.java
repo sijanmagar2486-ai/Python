a354c1a5e10bc0e9aea053cfdb26b64a57a5ff41
Sijan thapa magar <sijanmagar2486@gmail.com>
2025-12-11 20:03:26 +0545

    Sijan thapa magar

---
+++ week6tuto.java
@@ -0,0 +1,29 @@
+
+/**
+ * Write a description of class week6tuto here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class week6tuto
+{
+    public static void main(String[] args)
+    {
+        int [] age;
+        age = new int [5];
+        
+        System.out.println(age.length);
+        
+        age [0]=10;
+        age [1]=20;
+        age [2]=30;
+        age [3]=40;
+        age [4]=50;
+        
+        
+        int []agr={5,10,15,20,25};
+        System.out.println(agr.length);
+        System.out.println(agr[0]);
+        for (in)
+    }
+}
